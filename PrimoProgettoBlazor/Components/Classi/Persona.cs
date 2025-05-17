e51f2f662a8b330b5ba16ed9410dc4e41f048c72
Principale <Principale@MSI>
2025-05-17 18:36:30 +0200

    Add project files.

---
+++ PrimoProgettoBlazor/Components/Classi/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace PrimoProgettoBlazor.Components.Classi
+{
+    public class Persona
+    {
+        public static int Totale = 0; 
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età {  get; set; }
+
+        public Persona()
+        {
+            Totale += 1;
+        }
+    }
+}
